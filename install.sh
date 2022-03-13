3b835310b8358cc0a847d4732c7db47908ec62d2
Cfp <cfp@cfp.gotdns.ch>
2022-03-13 16:07:29 +0100

    Use nvm instead

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 sudo apt update -y
-sudo apt install git node npm -y
+sudo apt install git -y
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install node
+
 sudo git clone https://github.com/cfpwastaken/cfpos /CfpOS
 cd /CfpOS/cfpOS
 sudo npm install
